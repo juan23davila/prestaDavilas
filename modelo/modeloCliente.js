2e139e236de728c171e422aa911ffc3de19faf5a
Juan Davila <juan23davila@gmail.com>
2020-11-24 22:12:17 -0500

    First version of schema and add model Customer

---
+++ modelo/modeloCliente.js
@@ -0,0 +1,26 @@
+var modeloCliente = 
+function(
+    idCustomer,
+    identType,
+    numIdent,
+    custName,
+    custLastName,
+    cellPhone,
+    phone,
+    address,
+    mail,
+    registerDate,
+    active)
+{
+    this.idCustomer = idCustomer;
+    this.identType = identType;
+    this.numIdent = numIdent;
+    this.custName = custName;
+    this.custLastName = custLastName;
+    this.cellPhone = cellPhone;
+    this.phone = phone;
+    this.address = address;
+    this.mail = mail;
+    this. registerDate = registerDate;
+    this.active = active;
+}
